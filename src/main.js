c99d2781075c458a549bb25ed1a3f0485326631c
lxpeak <742907278@qq.com>
2022-11-07 16:01:45 +0800

    路由的基本使用

--- src/main.js
+++ src/main.js
@@ -1,20 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-//引入store
-//文件夹下是index.js文件的话就不需要写成'./store/index.js'了，vue会自动找index.js文件，
-//如果没有index.js文件就会报错，如果是其他文件名的话就需要指定文件名
-import store from './store'
+import VueRouter from 'vue-router'
+import router from './router'
 
 //关闭Vue的生产提示
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
 //创建vm
 new Vue({
   render: h => h(App),
-  store,
-  beforeCreate() {
-    //设置消息总线
-    Vue.prototype.$bus = this
-  },
+  router
 }).$mount('#app')
