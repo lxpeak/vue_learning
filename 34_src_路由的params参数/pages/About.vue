ad966ec619c6c0c759e2631e08b3aff40126a4b8
lxpeak <742907278@qq.com>
2022-11-30 18:24:25 +0800

    34

--- 34_src_路由的params参数/pages/About.vue
+++ 34_src_路由的params参数/pages/About.vue
@@ -1,16 +1,12 @@
 <template>
     <div>
-        <ul>
-            <li>{{$route.params.id}}</li>
-            <li>{{$route.params.title}}</li>
-        </ul>
+        <h2>我是About</h2>
     </div>
 </template>
 
 <script>
-
 export default {
-    name:"Detail",
+    //import引入的组件需要注入到对象中才能使用
     components: {},
     data() {
         //这里存放数据
@@ -34,6 +30,13 @@ export default {
     mounted() {
 
     },
+    beforeCreate() { }, //生命周期 - 创建之前
+    beforeMount() { }, //生命周期 - 挂载之前
+    beforeUpdate() { }, //生命周期 - 更新之前
+    updated() { }, //生命周期 - 更新之后
+    beforeDestroy() { }, //生命周期 - 销毁之前
+    destroyed() { }, //生命周期 - 销毁完成
+    activated() { }, //如果页面有keep-alive缓存功能，这个函数会触发
 }
 </script>
 <style scoped>
--- 34_src_路由的params参数/pages/News.vue
+++ 34_src_路由的params参数/pages/News.vue
@@ -1,16 +1,18 @@
 <template>
-    <div>
-        <ul>
-            <li>{{$route.params.id}}</li>
-            <li>{{$route.params.title}}</li>
-        </ul>
-    </div>
+    <ul>
+        <li>news001</li>
+        <li>news002</li>
+        <li>news003</li>
+    </ul>
 </template>
 
 <script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
 
 export default {
-    name:"Detail",
+    name: "News",
+
     components: {},
     data() {
         //这里存放数据
@@ -24,14 +26,6 @@ export default {
     watch: {},
     //方法集合
     methods: {
-
-    },
-    //生命周期 - 创建完成（可以访问当前this实例）
-    created() {
-
-    },
-    //生命周期 - 挂载完成（可以访问DOM元素）
-    mounted() {
 
     },
 }
