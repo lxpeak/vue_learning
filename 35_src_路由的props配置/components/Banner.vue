25e805f1ab384e4d4c1b128a12c9c980c22f028d
lxpeak <742907278@qq.com>
2022-12-01 01:46:52 +0800

    35路由的props配置

---
+++ 35_src_路由的props配置/components/Banner.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Vue Router Demo</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    data() {
+        //这里存放数据
+        return {
+
+        };
+    },
+}
+</script>
+<style scoped>
+
+</style>
---
+++ 35_src_路由的props配置/pages/Message.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="m in messageList" :key="m.id">
+                <!-- 建议用第二种 -->
+                
+                <!-- 第一种方式 -->
+                <!-- 跳转路由并携带params参数，to的字符串写法 -->
+                <!-- <router-link :to="`/home/message/detail/${m.id}/${m.title}`">{{ m.title }}</router-link> -->
+
+                <!-- 建议用第二种 -->
+                <!-- 第二种方式 -->
+                <!-- 跳转路由并携带params参数，to的对象写法 -->
+                <router-link :to="{
+                    // 传递params参数时不能用path只能用name
+                    name:'detail',
+                    params:{
+                        id:m.id,
+                        title:m.title,
+                    }
+                }">
+                    {{ m.title }}
+                </router-link>
+
+            </li>
+        </ul>
+        <hr>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Message",
+    components: {},
+    data() {
+        //这里存放数据
+        return {
+            messageList: [
+                { id: '001', title: '消息001' },
+                { id: '002', title: '消息002' },
+                { id: '003', title: '消息003' },
+                { id: '004', title: '消息004' },
+            ]
+        };
+    },
+}
+</script>
+<style scoped>
+
+</style>
