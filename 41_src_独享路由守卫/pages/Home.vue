699005b58994e8bcf235e02561659c15ac2ff5b8
lxpeak <742907278@qq.com>
2022-12-02 01:15:54 +0800

    41_src_独享路由守卫

---
+++ 41_src_独享路由守卫/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>Home组件内容</h2>
+        <div>
+            <ul>
+                <li>
+                    <router-link to="/home/news">News</router-link>
+                </li>
+                <li>
+                    <router-link to="/home/message">Message</router-link>
+                </li>
+            </ul>
+        </div>
+        <!-- include里填的是组件的名字，即<script>里的name值,不是文件名 -->
+        <keep-alive include="News">
+            <router-view></router-view>
+        </keep-alive>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    data() {
+        //这里存放数据
+        return {
+
+        };
+    },
+}
+</script>
+<style scoped>
+
+</style>
