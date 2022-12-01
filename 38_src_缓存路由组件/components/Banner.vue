99e39fbc8c7e822bd49b467ec963acd570b86564
lxpeak <742907278@qq.com>
2022-12-02 00:28:21 +0800

    Revert "Revert "缓存路由组件""
    This reverts commit db723489df2767ce97163375c14872a350f25e8b.

---
+++ 38_src_缓存路由组件/components/Banner.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>Vue Router Demo</h2>
+        <button @click="back">后退</button>
+        <button @click="forward">前进</button>
+        <button @click="go3">前进3步</button>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    data() {
+        //这里存放数据
+        return {
+
+        };
+    },
+    methods: {
+        back() {
+            this.$router.back();
+        },
+        forward() {
+            this.$router.forward()
+        },
+        go3(){
+            this.$router.go(3)
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
