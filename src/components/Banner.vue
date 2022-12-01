9a40501652a7ce6bdfcf13fb87370643df15ea37
lxpeak <742907278@qq.com>
2022-12-02 00:13:52 +0800

    Revert "编程式路由导航"
    This reverts commit 9e9245247d36b2d5525365b8005842aa6062e81c.

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,9 +1,6 @@
 <template>
     <div>
         <h2>Vue Router Demo</h2>
-        <button @click="back">后退</button>
-        <button @click="forward">前进</button>
-        <button @click="go3">前进3步</button>
     </div>
 </template>
 
@@ -16,17 +13,6 @@ export default {
 
         };
     },
-    methods: {
-        back() {
-            this.$router.back();
-        },
-        forward() {
-            this.$router.forward()
-        },
-        go3(){
-            this.$router.go(3)
-        }
-    }
 }
 </script>
 <style scoped>
